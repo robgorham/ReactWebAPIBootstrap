96bf4fc0e014a42398ca832fec0242aa918ede0c
Robert Gorham <robsteeliano@gmail.com>
2018-09-26 15:14:35 -0400

    Spelling Correction

--- rawapi.ui/src/components/Welcome.tsx
+++ rawapi.ui/src/components/Welcome.tsx
@@ -6,7 +6,7 @@ function Welcome() {
     return (<Row>
         <Col md={6} mdOffset={3}>
             <h3>Welcome and Hello!</h3>
-            <p>This Project is created in the hopes of making a boilerplate that will solve time</p>
+            <p>This Project is created in the hopes of making a boilerplate that will save time</p>
             <p>for developers to stand up a project with basic authentication built in</p>
         </Col>
     </Row>);
