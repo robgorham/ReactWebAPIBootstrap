c3fa72d683fa5752b6e3cd2ee78459ddb4db58d7
Robert Gorham <robsteeliano@gmail.com>
2018-09-26 11:40:09 -0400

    Added the typescript definitions for the rest of the project (redux, react-router..) Changed the layout to use the react bootstrap layout.

--- rawapi.ui/src/App.tsx
+++ rawapi.ui/src/App.tsx
@@ -1,24 +1,29 @@
 import * as React from 'react';
 import FetchTester from './components/fetchTester';
 import './App.css';
-
+import { Grid, Col, Row } from 'react-bootstrap';
 import logo from './logo.svg';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-            <FetchTester myUrl='localhost:50933' />
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <Grid >
+                <Row>
+                    <Col md={12}
+                         className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <h1 className="App-title">Welcome to React</h1>
+                        </header>
+                        <p className="App-intro">
+                            To get started, edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                        <FetchTester myUrl='localhost:50933' />
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
 
 export default App;
