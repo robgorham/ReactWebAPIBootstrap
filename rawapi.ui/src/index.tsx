4d0ac429fe5494aee5e82c0d2378262c0ed1945b
Robert Gorham <robsteeliano@gmail.com>
2018-09-29 16:34:24 -0400

    starting to add reducers

--- rawapi.ui/src/index.tsx
+++ rawapi.ui/src/index.tsx
@@ -4,9 +4,14 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from 'redux';
+import {Provider } from 'react-redux';
+
+const store = createStore(App);
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
+    <Provider store={store}>
+    <BrowserRouter><App /></BrowserRouter></Provider>,
     document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
--- rawapi.ui/src/reducers/index.tsx
+++ rawapi.ui/src/reducers/index.tsx
@@ -1,8 +1,18 @@
 ﻿import { URLChangeAction } from '../actions';
 import { StoreState } from '../types';
 import { MYURLCHANGE } from '../constants';
+import { combineReducers, Dispactch, Reducer } from 'redux';
+import {routerReducer} from 'react-router-redux';
 
-export function myURLchange(state: StoreState, action: URLChangeAction) :StoreState{
+
+
+export const reducers: Reducer<StoreState> = 
+    combineReducers<StoreState>({
+        router: routerReducer,
+
+    })
+
+export function configReducer(state: StoreState, action: URLChangeAction) :StoreState{
     if (action.type === MYURLCHANGE) {
         return {
             myUrl: action.myUrl
